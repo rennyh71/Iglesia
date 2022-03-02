1f79b3c96ff88001ebdae0e3475c55dd27ccb7e9
rennyh71 <rennyh71@gmail.com>
2022-03-02 00:20:04 -0400

    ajuste de posicion de video1.2

--- src/componentes/home.js
+++ src/componentes/home.js
@@ -19,7 +19,7 @@ export default class home extends React.Component {
               <iframe
                 width="80%"
                 height="100%"
-                src="https://www.youtube.com/embed/ST1X01R5mfQ"
+                src="https://www.youtube.com/embed/HrjcbojyBsk"
                 title="YouTube video player"
                 frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
