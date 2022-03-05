9111329bd9ce65e89399c42d1780e5c5f9363239
rennyh71 <rennyh71@gmail.com>
2022-03-04 21:03:27 -0400

    fondo

--- src/componentes/home.js
+++ src/componentes/home.js
@@ -16,7 +16,15 @@ export default class home extends React.Component {
           <div className={style.opacidad}>
             <section className={style.videos}>
               <h1>Bienvenida</h1>
-             <iframe width="80%" height="100%" src="https://www.youtube.com/embed/T6CKM-uH7wE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              <iframe
+                width="100%"
+                height="100%"
+                src="https://www.youtube.com/embed/T6CKM-uH7wE"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+              ></iframe>
             </section>
             <main className={style.contenido}>{<Noticias />}</main>
 
@@ -24,30 +32,30 @@ export default class home extends React.Component {
               <Card />
               <Card reverse={true} />
             </section>
+            <hr />
+            <footer className={style.footer}>
+              <div className={style.footerDiv}>
+                <a href="">HELLO@HELLOCREATIVIDAD.COM</a>
+                <a href="">Cookies</a>
+                <a href="">Política de privacidad</a>
+                <a href="">Aviso legal</a>
+              </div>
+              <div className={style.footerDiv}>
+                <a href="">SÍGUENOS EN</a>
+                <a href="">Facebook</a>
+                <a href="">Instagram</a>
+                <a href="">Twitter</a>
+              </div>
+              <div className={style.footerDiv}>
+                <a href="">HELLO CREATIVIDAD ES UNA MARCA REGISTRADA © 2021.</a>
+                <a href="">hellocreatividad.com</a>
+                <a href="">Diseño y desarrollo por Blavet</a>
+              </div>
+              <div className={style.footerDiv}>
+                <a href="">GRACIAS POR SU VISITA. ¡HASTA PRONTO!</a>
+              </div>
+            </footer>
           </div>
-
-          <footer className={style.footer}>
-            <div className={style.footerDiv}>
-              <a href="">HELLO@HELLOCREATIVIDAD.COM</a>
-              <a href="">Cookies</a>
-              <a href="">Política de privacidad</a>
-              <a href="">Aviso legal</a>
-            </div>
-            <div className={style.footerDiv}>
-              <a href="">SÍGUENOS EN</a>
-              <a href="">Facebook</a>
-              <a href="">Instagram</a>
-              <a href="">Twitter</a>
-            </div>
-            <div className={style.footerDiv}>
-              <a href="">HELLO CREATIVIDAD ES UNA MARCA REGISTRADA © 2021.</a>
-              <a href="">hellocreatividad.com</a>
-              <a href="">Diseño y desarrollo por Blavet</a>
-            </div>
-            <div className={style.footerDiv}>
-              <a href="">GRACIAS POR SU VISITA. ¡HASTA PRONTO!</a>
-            </div>
-          </footer>
         </div>
       </div>
     );
